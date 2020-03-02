09447f2eb78e965c8a76672be005cf7234e1413d
George Aoyagi <aoyagigeorge@gmail.com>
2020-03-02 13:44:28 -0800

    successfully got the mac os and python3 version to show

--- main.go
+++ main.go
@@ -8,15 +8,16 @@ import (
 //"sw_vers -productVersion" for current macOS   python3 --version for current python3version
 
 func main() {
-	path, err := exec.LookPath("Python")
-	errorCatch(err)
-	//osVer, err := exec.Command("sw_vers -productVersion").Output()
-	pyVer, err1 := exec.Command("/usr/bin/Python python3 --version", path).Output()
+	pyVer, err1 := exec.Command("python3", "--version").Output()
 	errorCatch(err1)
-	fmt.Printf("%s", pyVer)//"python3 version: %s\n", out)
 	
-}
+	osVer, err2 := exec.Command("sw_vers",  "-productVersion").Output()
+	errorCatch(err2)
 
+	fmt.Printf("current python3 version: %s", string(pyVer)) 
+	fmt.Printf("current Mac OS version: %s", string(osVer))
+
+}
 
 func errorCatch(err error) {
 	if err != nil {
